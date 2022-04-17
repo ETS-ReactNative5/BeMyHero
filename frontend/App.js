1e57576c75ded51445aebb85c4b290a3127feed5
Aditya <adityam0338@gmail.com>
2022-04-17 12:48:13 +0530

    added geolocation

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,41 @@
+import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Linking } from 'react-native';
+import { StyleSheet, Platform, Text, View, TextInput, Linking } from 'react-native';
 
 export default class Example extends Component {
-  const Signup = ()  => {
+  constructor(){  
+    super();  
+    this.state = {  
+        ready: false,  
+        where: {lat:null, lng:null},  
+        error: null  
+    }  
+  }  
+
+  componentDidMount(){  
+    let geoOptions = {  
+        enableHighAccuracy:false,  
+        timeOut: 20000, //20 second  
+      //  maximumAge: 1000 //1 second  
+    };  
+    this.setState({ready:false, error: null });  
+    navigator.geolocation.getCurrentPosition( this.geoSuccess, this.geoFailure,geoOptions);  
+  }
+
+  geoSuccess = (position) => {  
+    console.log(position.coords.latitude);  
+
+    this.setState({  
+        ready:true,  
+        where: {lat: position.coords.latitude,lng:position.coords.longitude }  
+    })  
+}  
+
+geoFailure = (err) => {  
+    this.setState({error: err.message});  
+}  
+
+  Signup = ()  => {
     return (
       <View>
           <TextInput />
@@ -10,7 +43,7 @@ export default class Example extends Component {
     );
   }
 
-  const Auth = () => {
+  Auth = () => {
     return(
       <View style = {styles.authwrap}>
         <TextInput style = {styles.texts} type="text" placeholder='Email Address'/>
@@ -24,7 +57,16 @@ export default class Example extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Auth />
+      {this.Auth()}
+
+      { this.state.ready && (  
+                    <Text style={styles.big}>  
+                        Latitude: {this.state.where.lat}  
+                        Longitude: {this.state.where.lng}  
+                    </Text>  
+          )}
+
+
       </View>
     );
   }
